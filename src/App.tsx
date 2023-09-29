d3993a651b03ac8f7aec7a35d45b56b39bf4df75
dianabreik <diana.sam341@gmail.com>
2023-09-29 15:18:09 +0200

    add a header to the page and a menu bar to navigate between pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,20 @@ import {Character, charactersResponse} from "./characters.ts";
 import CharacterGallery from "./CharacterGallery.tsx";
 import {Route, Routes} from "react-router-dom";
 import Welcome from "./Welcome.tsx";
+import Header from "./Header.tsx";
 
 function App() {
 
     const [characters, setCharacters] = useState<Character[]>(charactersResponse.results)
 
     return (
-        <>
+        <div>
+            <Header/>
             <Routes>
                 <Route path={"/*"} element={<Welcome/>}/>
                 <Route path={"/characters"} element={<CharacterGallery characters={characters}/>}/>
             </Routes>
-        </>
+        </div>
     )
 }
 
---
+++ src/Header.tsx
@@ -0,0 +1,20 @@
+import {Link} from "react-router-dom";
+import './App.css'
+export default function Header(){
+    return(
+        <header className={"header"}>
+
+            <h1>
+                RICK & MORTY
+            </h1>
+
+            <nav>
+                <ul>
+                    <li><Link to={"/*"}> HOME </Link></li>
+                    <li><Link to={"/characters"}> Gallery </Link></li>
+                </ul>
+            </nav>
+
+        </header>
+    )
+}
